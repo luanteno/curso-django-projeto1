c32d76e41f7f71ba3401986b2fa31fee195c01d5
Luan Tenório <luanteno@gmail.com>
2023-08-17 13:49:05 -0300

    Definição de namespaces - Global / Local

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps:
+    'receitas',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'projeto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'global_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- receitas/views.py
+++ receitas/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home nova')
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
